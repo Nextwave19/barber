a1a0d92fad2da6bbde28571748881fa047a80998
Nextwave19 <nextwaveaiandweb@gmail.com>
2025-07-21 21:30:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,7 @@ def login():
 
         admin_user = os.getenv("ADMIN_USERNAME")
         admin_pass = os.getenv("ADMIN_PASSWORD")
-        print("Admin from env:", admin_user)
-        print("Password from env:", admin_pass)
-
+        
         # אם זה האדמין – נדרשת סיסמה
         if username == admin_user:
             if password != admin_pass:
