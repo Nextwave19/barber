548a879b9aa6f39faa3f79aa5a89174c27422982
Nextwave19 <nextwaveaiandweb@gmail.com>
2025-07-23 11:45:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def index():
 def login():
     error = None
     admin_user = os.environ.get('ADMIN_USERNAME')
-    admin_password =1234
+    admin_password ="1234"
     if request.method == 'POST':
         username = request.form['username'].strip()
         password = request.form.get('password', '')
