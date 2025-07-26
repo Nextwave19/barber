5d9cd70edbe0880067f202723ed63666a155e93d
Nextwave19 <nextwaveaiandweb@gmail.com>
2025-07-27 00:55:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ from email.message import EmailMessage
 from datetime import datetime, timedelta
 from collections import defaultdict
 
+def load_json(filename):
+    if not os.path.exists(filename):
+        return {}
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def save_json(filename, data):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY") or "default_secret_key"
